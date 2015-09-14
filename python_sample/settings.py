e783aab82b3b0fc1862ad63f6889b5f977b69b71
Albert.Sun <atschx@gmail.com>
2015-09-14 14:47:00 +0800

    python redis init

---
+++ python_sample/settings.py
@@ -0,0 +1,19 @@
+
+
+# : we recommend that you connect to Redis via Twemproxy
+SAMPLE_REDIS_CONFIG = {
+    'default': {
+        'host': '127.0.0.1',
+        'port': 6379,
+        'db': 0,
+        'password': None
+    },
+}
+
+#基础mysql配置
+
+#redis配置
+
+#基础cassandra配置
+
+CASSANDRA_HOSTS = ['localhost']
---
+++ python_sample/test.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+#coding:utf-8
+#author:albert
+#date:2015-09-14
+
+from python_sample.storage.
